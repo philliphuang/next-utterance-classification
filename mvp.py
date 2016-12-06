09dae47a1620b46950bdfe79231e3298d53a4853
Harry Xue <wxue@college.harvard.edu>
2016-12-06 16:52:29 -0500

    Pushing before updating

--- mvp.py
+++ mvp.py
@@ -15,16 +15,16 @@ I need to poop'''
 train_data = train.split('\n')
 
 while True:
-	s = raw_input('Message: ')
+    s = raw_input('Message: ')
 
-	dists = []
+    dists = []
 
-	for past_m in train_data:
-		dists.append(editdistance.eval(past_m, s))
+    for past_m in train_data:
+        dists.append(editdistance.eval(past_m, s))
 
-	best_ind = min(enumerate(dists), key=itemgetter(1))[0] 
+    best_ind = min(enumerate(dists), key=itemgetter(1))[0] 
 
-	if best_ind == len(train_data):
-		print 'Pizzabot: ' + train_data[0]
-	else: 
-		print 'Pizzabot: ' + train_data[best_ind + 1]
+    if best_ind == len(train_data):
+        print 'Pizzabot: ' + train_data[0]
+    else: 
+        print 'Pizzabot: ' + train_data[best_ind + 1]
